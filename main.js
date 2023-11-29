6d14288c2bf7790c854a41107bb611271814ae72
ars1992 <alessandro.salomon@gmx.de>
2023-11-29 12:20:31 +0100

    ende 3

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log(THREE);
+"use strict";
+
+function main(){
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
+        45,
+        window.innerHeight / window.innerHeight,
+        1,
+        1000,
+    )
+
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+
+    document.querySelector(".webgl").appendChild(renderer.domElement)
+    renderer.render(scene, camera)
+    
+}
+
+main()
