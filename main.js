550be42d421ebc0c6f4177ce0c9461115d59c343
ars1992 <alessandro.salomon@gmx.de>
2023-11-29 13:05:30 +0100

    5. Der Boden

--- main.js
+++ main.js
@@ -4,17 +4,22 @@ function main(){
     const scene = new THREE.Scene()
     const box = generateBox(1, 1, 1)
     box.translateZ(-5)
+    box.position.y = box.geometry.parameters.height / 2
+
+    const floor = generateFloor(10, 20)
+    floor.rotation.x = Math.PI / 2
     scene.add(box)
+    scene.add(floor)
 
     const camera = new THREE.PerspectiveCamera(
         45,
-        window.innerHeight / window.innerHeight,
+        window.innerWidth / window.innerHeight,
         1,
         1000,
     )
-    camera.position.x = 0
+    camera.position.x = 1
     camera.position.y = 5
-    camera.position.z = 0
+    camera.position.z = 5
     camera.lookAt(new THREE.Vector3(0, 0, -5))
 
     const renderer = new THREE.WebGLRenderer()
@@ -23,6 +28,16 @@ function main(){
     document.querySelector(".webgl").appendChild(renderer.domElement)
     renderer.render(scene, camera)
     
+}
+
+function generateFloor(w, d){
+    const geometry = new THREE.PlaneGeometry(w, d)
+    const material = new THREE.MeshBasicMaterial({
+        color: 0x00ff00,
+        side: THREE.DoubleSide,
+    })
+    const mesh = new THREE.Mesh(geometry, material)
+    return mesh
 }
 
 function generateBox(w, h, d){
