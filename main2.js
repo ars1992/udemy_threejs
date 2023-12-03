7a3cc6cf206194f4fc53b8a3b63f6b9fc3aa0cba
ars1992 <alessandro.salomon@gmx.de>
2023-12-03 16:16:04 +0100

    19. Partikeleffekte

--- main2.js
+++ main2.js
@@ -1,12 +1,12 @@
 const keyboard = new THREEx.KeyboardState()
 const clock = new THREE.Clock()
 
-function init(){
+function init() {
     const scene = new THREE.Scene();
 
     const leftLight = genareteSpotLight("rgb(255, 200, 200)", 0.5)
     leftLight.name = "leftLight"
-    
+
     const rightLight = genareteSpotLight("rgb(255, 200, 200)", 1.5)
     rightLight.name = "rightLight"
 
@@ -24,7 +24,7 @@ function init(){
     scene.background = reflectionCube
     scene.add(leftLight)
     scene.add(rightLight)
-    
+
 
     const camera = new THREE.PerspectiveCamera(
         45,
@@ -39,29 +39,28 @@ function init(){
 
     const objloader = new THREE.OBJLoader()
     const textureloader = new THREE.TextureLoader()
-    objloader.load("img/wolf_model/Wolf_obj.obj", function(object){
+    objloader.load("img/wolf_model/Wolf_obj.obj", function (object) {
         const body = textureloader.load("img/wolf_model/textures/Wolf_Body.jpg")
         const fur = textureloader.load("img/wolf_model/textures/Wolf_Fur.jpg")
 
-        const bodyMat = new THREE.MeshStandardMaterial({color: "rgb(255, 255, 255)"})
-        const furMat = new THREE.MeshStandardMaterial({color: "rgb(255, 255, 255)"})
+        const bodyMat = new THREE.MeshStandardMaterial({ color: "rgb(255, 255, 255)" })
+        const furMat = new THREE.MeshStandardMaterial({ color: "rgb(255, 255, 255)" })
 
-        object.traverse(function(child){
-            if(child.name === 'Wolf_obj_body_Cube.001'){
+        object.traverse(function (child) {
+            if (child.name === 'Wolf_obj_body_Cube.001') {
                 child.material = bodyMat
                 bodyMat.roughness = 1
                 bodyMat.map = body
                 bodyMat.metalness = 0
             }
 
-            if(child.name === 'Wolf_obj_fur_Cube.002'){
+            if (child.name === 'Wolf_obj_fur_Cube.002') {
                 child.material = furMat
                 furMat.roughness = 1
                 furMat.map = fur
                 furMat.metalness = 0
             }
         })
-
 
         object.scale.x = 4
         object.scale.y = 4
@@ -70,6 +69,24 @@ function init(){
 
         scene.add(object)
     })
+
+    const particleSystem = new THREE.Geometry()
+    const particleMat = new THREE.PointsMaterial({
+        color: "rgb(255, 255, 255)",
+        size: 0.5,
+        map: new THREE.TextureLoader().load("img/particle.jpg"),
+        transparent: true,
+        blending: THREE.AdditiveBlending,
+        depthWrite: false
+    })
+
+    const particelAmount = 50_000
+    for (let i = 0; i < particelAmount; i++) {
+        const particle = new THREE.Vector3(Math.random() * 200 - 100, Math.random() * 200 - 100, Math.random() * 200 - 100)
+        particleSystem.vertices.push(particle)
+    }
+    const particles = new THREE.Points(particleSystem, particleMat)
+    scene.add(particles)
 
     const renderer = new THREE.WebGLRenderer()
     renderer.shadowMap.enabled = true
@@ -126,4 +143,4 @@ function update(renderer, scene, camera, controls) {
     })
 }
 
- console.log(init())
+console.log(init())
